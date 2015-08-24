2c691b40bf42e4094963dd1e11cbaba6ee69b027
abhikumar90 <abhi.kumar90@hotmail.com>
2015-08-24 22:06:11 +0530

    file added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KreatioBook::Application.config.secret_key_base = '8762661623a5569ed47a166ebd20bd83a7332662b2b882526ebf7fb11f64209b7a2984113ad58998eb2bfda179e149ed6cb11f8f52f9243fb5ecd72741bb974b'
--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,102 @@
-Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+KreatioBook::Application.routes.draw do
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #get "books/new"
+  match "/new" => "books#new", via: :get
+  
+  
+  #get "books/about"
+  match "/about" => "books#about", via: :get
+  
+  #get "books/boks"
+  match "/books" => "books#books", via: :get  
+  
+  #get "books/category"
+   match "/category" => "books#category", via: :get
+   
+  #get "books/uploadbook"
+   match "/uploadbook" => "books#uploadbook", via: :get
+   
+  #get "books/contact"
+   match "/contact" => "books#contact", via: :get
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get "books/find"
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  
+  post "books/create"
+  post "upload/uploadFile"
+  post "books/check_exist"
+  post "books/download_book"
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  root :to => "books#new"
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  get "log_in" => "sessions#new", :as => "log_in"
+  
+  get "log_out" => "sessions#destroy", :as => "log_out"
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+  get "sign_up" => "users#login", :as => "sign_up"
+  #root :to => "users#login"
+  resources :users
+  resources :sessions
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+
+
+
+
+
+# The priority is based upon order of creation: first created -> highest priority.
+# See how all your routes lay out with "rake routes".
+
+# You can have the root of your site routed with "root"
+# root 'welcome#index'
+
+# Example of regular route:
+#   get 'products/:id' => 'catalog#view'
+
+# Example of named route that can be invoked with purchase_url(id: product.id)
+#   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+# Example resource route (maps HTTP verbs to controller actions automatically):
+#   resources :products
+
+# Example resource route with options:
+#   resources :products do
+#     member do
+#       get 'short'
+#       post 'toggle'
+#     end
+#
+#     collection do
+#       get 'sold'
+#     end
+#   end
+
+# Example resource route with sub-resources:
+#   resources :products do
+#     resources :comments, :sales
+#     resource :seller
+#   end
+
+# Example resource route with more complex sub-resources:
+#   resources :products do
+#     resources :comments
+#     resources :sales do
+#       get 'recent', on: :collection
+#     end
+#   end
+
+# Example resource route with concerns:
+#   concern :toggleable do
+#     post 'toggle'
+#   end
+#   resources :posts, concerns: :toggleable
+#   resources :photos, concerns: :toggleable
+
+# Example resource route within a namespace:
+#   namespace :admin do
+#     # Directs /admin/products/* to Admin::ProductsController
+#     # (app/controllers/admin/products_controller.rb)
+#     resources :products
+#   end
 end
